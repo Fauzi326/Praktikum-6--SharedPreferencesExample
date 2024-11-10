4ffee80e505f731b82876c4f452e8e788a477640
ACER <m.fauzi30052004@gmail.com>
2024-11-10 22:59:40 +0700

    add MainActivity.kt

--- app/src/main/java/muhammadfauzi/polbeng/ac/id/sharedpreferencesexample/MainActivity.kt
+++ app/src/main/java/muhammadfauzi/polbeng/ac/id/sharedpreferencesexample/MainActivity.kt
@@ -1,6 +1,7 @@
 package muhammadfauzi.polbeng.ac.id.sharedpreferencesexample
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -8,20 +9,23 @@ import muhammadfauzi.polbeng.ac.id.sharedpreferencesexample.databinding.Activity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val pref = getPreferences(Context.MODE_PRIVATE)
+
+        val filename = "$packageName.TESTFILE"
+        val pref = getSharedPreferences(filename, Context.MODE_PRIVATE)
+
         binding.btnSave.setOnClickListener {
             val editor = pref.edit()
-            editor.putString("firstName",
-                binding.etFirstName.text.toString())
-            editor.putString("lastName",
-                binding.etLastName.text.toString())
+            editor.putString("firstName", binding.etFirstName.text.toString())
+            editor.putString("lastName", binding.etLastName.text.toString())
             editor.apply()
             Toast.makeText(this, "Saved Data!", Toast.LENGTH_LONG).show()
         }
+
         binding.btnLoad.setOnClickListener {
             val firstName = pref.getString("firstName", "")
             val lastName = pref.getString("lastName", "")
@@ -30,7 +34,13 @@ class MainActivity : AppCompatActivity() {
             binding.etLastName.setText(lastName)
             binding.tvOutput.text = output
         }
+
+        binding.btnSecondActivity.setOnClickListener {
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
+
     override fun onResume() {
         super.onResume()
         binding.etFirstName.setText("")
